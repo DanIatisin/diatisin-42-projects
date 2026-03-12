736cbc94367918ec9fa92e2b0fbb5db9d7e22654
Dan Iatisin <diatisin@e3r3p3.42roma.it>
2026-03-12 16:31:36 +0100

    finished ex00, 01 && 02

---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diatisin <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/12 16:21:52 by diatisin          #+#    #+#             */
+/*   Updated: 2026/03/12 16:25:23 by diatisin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void ft_print_reverse_alphabet(void){
+
+	char c = 'z';
+
+	while (c <= 'z' && c >= 'a'){
+		
+		write(1, &c, 1);
+		c--;
+	
+	}
+
+}
+
+
+int main(){
+
+	
+	ft_print_reverse_alphabet();
+	return 0;
+}
