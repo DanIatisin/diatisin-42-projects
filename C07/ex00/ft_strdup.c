87ddde79664c98396cebc6fefc801180701bbd63
DanIatisin <iatisindaniel@gmail.com>
2026-04-01 21:59:55 +0200

    some changes

---
+++ C07/ex00/ft_strdup.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while(str[i] != '\0')
+		i++;
+	return (i);
+}
+char	*ft_strdup(char *src)
+{
+	int	i;
+	int	len;
+	char	*dest;
+
+	i = 0;
+	len = ft_strlen(src);
+	dest = malloc(sizeof(char) * len);
+
+	while(src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/*int	main(int argc, char **argv)
+{
+	char	*dest;
+	int 	i;
+
+	if(argc < 2)
+		return (1);
+	i = 1;
+	dest = ft_strdup(argv[i]);
+	printf("%s\n", dest);
+	free(dest);
+	return (0);
+}*/
---
+++ C07/ex03/ft_strjoin.c
@@ -0,0 +1,9 @@
+char	*ft_strjoin(int size, char **strs, char *sep)
+{
+	char	*final;
+
+	if(size == 0)
+		return (final);
+	
+
+}
