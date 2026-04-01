17cd16a981b4686aa02d69941efdee90cece7e9d
Dan Iatisin <diatisin@e3r11p6.42roma.it>
2026-04-01 12:09:04 +0200

    Finished C05

--- C05/ex00/ft_iterative_factorial.c
+++ C05/ex00/ft_iterative_factorial.c
@@ -1,13 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diatisin <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/01 11:52:59 by diatisin          #+#    #+#             */
+/*   Updated: 2026/04/01 11:54:33 by diatisin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 int	ft_iterative_factorial(int nb)
 {
 	int	result;
-	
+
 	result = 1;
-	if(nb < 0)
+	if (nb < 0)
 		return (0);
-	else if(nb == 0)
+	else if (nb == 0)
 		return (1);
-	while(nb >= 1)
+	while (nb >= 1)
 	{
 		result = result * nb ;
 		nb--;
