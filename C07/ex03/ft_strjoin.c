9db98fe58f6c8cde8c5a2f8088b9818f5e6788f2
Dan Iatisin <diatisin@e3r9p6.42roma.it>
2026-04-02 15:32:23 +0200

    BOH

--- C07/ex03/ft_strjoin.c
+++ C07/ex03/ft_strjoin.c
@@ -1,9 +1,46 @@
-char	*ft_strjoin(int size, char **strs, char *sep)
+int	strlen(char *str)
 {
-	char	*final;
+	int	i;
 
-	if(size == 0)
-		return (final);
-	
-
+	i = 0;
+	while(str[i] != '\0')
+		i++;
+	return(i);
 }
+
+char	*search_world(char *sep, char *str)
+{
+	char	*result;
+	int	i;
+
+	i = 0;
+	while(sep[i])
+	{
+		j = 0;
+		while (str[j] != '\0')
+		{
+			if(str[j] != src[i])
+				result[j] = str[j];
+			j++;
+		}
+		i++;
+	}
+	return (result);
+}
+
+/*char	*ft_strjoin(int size, char **strs, char *sep)
+{
+	char	*result;
+	char	sep_len;
+	char	str_len;
+
+	if (size == 0)
+	{
+		result = malloc(1);
+	if (result)
+		result[0] = '\0';
+	return (resutl);
+	}
+}*/
+
+:
