5b025d007c0e37c0abf03afbbc2c82b37298e022
Dan Iatisin <diatisin@c3r2s2.42roma.it>
2026-05-28 14:52:16 +0200

    headers & norm

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,17 +6,12 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/21 14:12:38 by diatisin          #+#    #+#             */
-/*   Updated: 2026/05/28 14:39:46 by marvin           ###   ########.fr       */
+/*   Updated: 2026/05/28 14:47:53 by diatisin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-<<<<<<< HEAD
-/*#include <stdio.h>
 #include "libft.h"
-#include <stdlib.h>*/
 
-=======
->>>>>>> aba778033d03c1f076f7fa6d63f8ebbfd99addb4
 void	*memcpy(void *dest, const void *src, size_t n)
 {
 	size_t				i;
