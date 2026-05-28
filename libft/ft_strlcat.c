804a4708fb3eaaf6ca65273a9a16924f347062e6
DanIatisin <iatisindaniel@gmail.com>
2026-05-28 09:49:20 +0200

    asas

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -1,5 +1,5 @@
 #include "libft.h"
-size_t	*ft_strlcat(char *dst, const char *src, size_t size)
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
 	size_t	dest_len;
 	size_t	src_len;
@@ -12,7 +12,7 @@ size_t	*ft_strlcat(char *dst, const char *src, size_t size)
 	{
 		return(size + src_len);
 	}
-	while (src[i] != '\0' && (dest_len + i) < size - i)
+	while (src[i] != '\0' && (dest_len + i) < size - 1)
 	{
 		dest[dest_len + i] = src[i];
 		i++;
